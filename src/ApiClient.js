70aaae7b7ec5f9102c49dd197061eb41c3c712e6
Alyzande <44140612+Alyzande@users.noreply.github.com>
2022-06-14 12:24:02 +0100

    minor

--- src/ApiClient.js
+++ src/ApiClient.js
@@ -9,7 +9,7 @@ export class ApiClient {
         }
     }
 
-    getItems(url) {
+    getWeatherList(url) {
         return axios.get(url)
             .then(this.responseStatusCheck)
             .catch((error) => {
@@ -17,22 +17,10 @@ export class ApiClient {
             });
     }
 
-getWeather(){
-    return this.getItems(`https://api.openweathermap.org/data/3.0/onecall?lat=53.38&lon=-1.47&exclude=current,minutely,hourly,alerts&units=metric&appid=10f8ebf1d0c06f8e93882e24c19e88eb`)
-    //will pass it lat/lon once it works for this latlon
-};
-
-    // getAuthors(skip = 0, limit = 20) {
-    //     return this.getItems(`https://api.quotable.io/authors?skip=${skip}&limit`)
-    // };
-
-    // getQuoteByAuthor(authorId) {
-    //     return this.getItems(`https://api.quotable.io/quotes?authorId=${authorId}`);
-    // };
-
-    // getQuote() {
-    //     return this.getItems("https://api.quotable.io/random")
-    // };
+    getWeather(){
+        return this.getWeatherList(`https://api.openweathermap.org/data/3.0/onecall?lat=53.38&lon=-1.47&exclude=current,minutely,hourly,alerts&units=metric&appid=10f8ebf1d0c06f8e93882e24c19e88eb`)
+        //will pass it lat/lon once it works for this latlon
+    }
 
 
 }
--- src/WeatherCard.js
+++ src/WeatherCard.js
@@ -2,16 +2,28 @@ import React, { useState } from "react";
 import './weathercard.css';
 import { ApiClient } from "./ApiClient";
 
+
 function WeatherCard() {
 
 const weatherInfo = useState({
   weathersDate: "a date passed as a string",
   weathersImage: "image placeholder",
-  degreeDisplay: "Number",
+  degreeDisplay: 0,
   weatherText: "long string",
-  windInfo: "Another Number",
+  windInfo: 0,
 });
+
 const apiClient = new ApiClient();
+
+apiClient.getWeather()
+  console.log(getWeather)
+  .then((jsonResponse) =>{
+  console.log("response", jsonResponse.data);
+  getWeather(jsonResponse.data);
+})
+.catch((error) => {
+  console.log (error);
+})
 
 
 const apistring ="https://api.openweathermap.org/data/3.0/onecall?lat=53.38&lon=-1.47&exclude=current,minutely,hourly,alerts&units=metric&appid=10f8ebf1d0c06f8e93882e24c19e88eb";
