cfe07d08b9cd12a04a1368ffcd0445efe29b2a93
Alyzande <44140612+Alyzande@users.noreply.github.com>
2022-06-14 11:28:35 +0100

    weathercard

--- src/WeatherCard.js
+++ src/WeatherCard.js
@@ -1,20 +1,17 @@
-import React, { useEffect } from "react";
+import React, { useState } from "react";
 import './weathercard.css';
 import { ApiClient } from "./ApiClient";
 
 function WeatherCard() {
 
-useEffect(() => {
-    let response = fetch("url");
-
-weatherInfo({
-    weathersDate: "a date passed as a string",
-    weathersImage: "image placeholder",
-    degreeDisplay: "Number",
-    weatherText: "long string",
-    windInfo: "Another Number", 
-}); 
-}, []);
+const weatherInfo = useState({
+  weathersDate: "a date passed as a string",
+  weathersImage: "image placeholder",
+  degreeDisplay: "Number",
+  weatherText: "long string",
+  windInfo: "Another Number",
+});
+const apiClient = new ApiClient();
 
 
 const apistring ="https://api.openweathermap.org/data/3.0/onecall?lat=53.38&lon=-1.47&exclude=current,minutely,hourly,alerts&units=metric&appid=10f8ebf1d0c06f8e93882e24c19e88eb";
