b97238497c6b12f02dad4081cf9924bc39953fae
Alyzande <44140612+Alyzande@users.noreply.github.com>
2022-06-14 10:19:48 +0100

    starting

---
+++ src/WeatherCard.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function WeatherCard() {
+  return (
+    <div className="WeatherCard">
+      weathersDate, image, degreeDisplay, weatherText, windInfo
+    </div>
+  );
+}
+
+export default WeatherCard;
