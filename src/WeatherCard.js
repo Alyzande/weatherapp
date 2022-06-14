fb6310ee8d6031c76856c82d97883497ff94238b
Alyzande <44140612+Alyzande@users.noreply.github.com>
2022-06-14 12:54:14 +0100

    lat lon

--- src/WeatherCard.js
+++ src/WeatherCard.js
@@ -15,18 +15,18 @@ const weatherInfo = useState({
 
 const apiClient = new ApiClient();
 
-apiClient.getWeather()
-  console.log(getWeather)
+apiClient.getWeather(53.383331,-1.466667)
+  //console.log(getWeather)
   .then((jsonResponse) =>{
   console.log("response", jsonResponse.data);
-  getWeather(jsonResponse.data);
+  //getWeather(jsonResponse.data);
 })
 .catch((error) => {
   console.log (error);
 })
 
 
-const apistring ="https://api.openweathermap.org/data/3.0/onecall?lat=53.38&lon=-1.47&exclude=current,minutely,hourly,alerts&units=metric&appid=10f8ebf1d0c06f8e93882e24c19e88eb";
+//const apistring ="https://api.openweathermap.org/data/3.0/onecall?lat=53.38&lon=-1.47&exclude=current,minutely,hourly,alerts&units=metric&appid=10f8ebf1d0c06f8e93882e24c19e88eb";
 
   return (
     <div className="WeatherCard">
