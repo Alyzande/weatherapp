08bc0b09da69db2096fb56d323e7e5f87f047f2a
Alyzande <44140612+Alyzande@users.noreply.github.com>
2022-06-15 14:01:18 +0100

    styling

--- src/WeatherCard.js
+++ src/WeatherCard.js
@@ -5,20 +5,22 @@ import 'moment-timezone';
 
 
 function WeatherCard(props) {
+  const degreeRounded =  Math.round(props.degreeDisplay, 1);
+  const dateParsed = (props.weathersDate * 1000);
+  const windSpeed = Math.round(props.windInfo, 1);
+  const weatherImgSrc = "http://openweathermap.org/img/wn/" + (props.weathersImage) + "@2x.png";
 
   return (
     <div className="WeatherCard">
       <span id="datespan">
-      <Moment format="ddd D">
-        {props.weathersDate} 
-      </Moment>
-      </span>imagehere
-      {props.weathersImage} 
-      <span id="degreedisplay">
-      {props.degreeDisplay}°C
+      <Moment format="ddd D">{dateParsed}</Moment>
       </span>
-      <span>weathertexthere{props.weatherText} </span>  
-       Wind Speed <span id="windspeed">{props.windInfo}mph</span>
+      <img src={weatherImgSrc} alt={props.weatherText} />
+      <span id="degreedisplay">
+      {degreeRounded}°C
+      </span>
+      <span id="weathertext">PH{props.weatherText}. </span>  
+       Wind:<span id="windspeed">{windSpeed}</span>m/s
     </div>
   );
 }
